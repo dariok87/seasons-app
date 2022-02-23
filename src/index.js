e264176ff3287ee9b009ea24332fe32f9d2ede4a
dariok87 <dkemenovic@gmail.com>
2022-02-23 19:29:27 +0100

    Delete functional component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err),
-  );
-
-  return <div>Latitude: </div>
-};
+import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
   render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+  
     return <div> Latitude: </div>;
   }
 }
