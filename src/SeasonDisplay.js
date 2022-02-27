d7823b8c94033379dc62e12eb4d113d9698c3555
dariok87 <dkemenovic@gmail.com>
2022-02-27 12:14:46 +0100

    Determine which season it is

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const SeasonDisplay = (props) => {
-  console.log(props.latitude)
-  return <div>Season Display</div>
+const getSeason = (latitude, month) => {
+  if (month > 2 && month < 9) {
+    return latitude > 0 ? 'summer' : 'winter'; // Are we in northern hemisphere
+  } else {
+    return latitude > 0 ? 'winter' : 'summer';
+  }
 }
 
-export default SeasonDisplay
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.latitude, new Date().getMonth());
+
+  console.log(season);
+
+  return <div>{season}</div>
+}
+
+export default SeasonDisplay;
