43b251a33017eca99afe96dd418bd01a6d18b6ae
dariok87 <dkemenovic@gmail.com>
2022-03-05 14:16:30 +0100

    Refactor with configuration object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Lets go to the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Lets go inside',
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (latitude, month) => {
   if (month > 2 && month < 9) {
     return latitude > 0 ? 'summer' : 'winter'; // Are we in northern hemisphere
@@ -10,15 +21,14 @@ const getSeason = (latitude, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.latitude, new Date().getMonth());
-  const text = season === 'winter' ? 'Brrr, it is chilly' : 'oh, it is very hot';
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];  // {text,iconName}
 
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
