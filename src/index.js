eba2d7cb07053e107e8a3b6467b68d9bc4ec3d81
dariok87 <dkemenovic@gmail.com>
2022-03-06 10:32:15 +0100

    Create helper function to avoid conditionals in render

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     )
   }
 
-  render() {
+  renderContent () {
     if (this.state.errorMessage && !this.state.latitude) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -23,6 +23,12 @@ class App extends React.Component {
     }
 
     return <Spinner message='Please accept location request'/>;
+  }
+
+  render() {
+    return(
+      <div>{this.renderContent()}</div>
+    ); 
   }
 }
 
